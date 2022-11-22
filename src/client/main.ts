284c892f728fb7619a77f5e0c6cff8e00ae92120
Jim Blackler <jimblackler@google.com>
2022-11-22 16:53:00 +0000

    Use functions for layout components.

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,7 @@
 import {defaultKeymap} from "@codemirror/commands"
 import {EditorView, keymap, lineNumbers} from "@codemirror/view"
 import {basicLight} from 'cm6-theme-basic-light'
-import {ComponentContainer, GoldenLayout, LayoutConfig} from 'golden-layout';
+import {GoldenLayout, LayoutConfig} from 'golden-layout';
 import {JSONEditor, Mode} from 'vanilla-jsoneditor'
 import {assertNotNull} from '../common/check/null';
 import './style.css'
@@ -47,44 +47,37 @@ const layoutConfig: LayoutConfig = {
 };
 const layout = new GoldenLayout(container);
 
-layout.registerComponentConstructor('testComponent', class {
-  constructor(container: ComponentContainer) {
-    const h2 = document.createElement('h2');
-    h2.append('Test');
-    container.element.append(h2);
+layout.registerComponentFactoryFunction('testComponent', container => {
+  const h2 = document.createElement('h2');
+  h2.append('Test');
+  container.element.append(h2);
+});
+
+layout.registerComponentFactoryFunction('editor', container => {
+  const data: any = {};
+  for (const key in navigator) {
+    data[key] = (navigator as any)[key];
   }
-}, false);
-
-layout.registerComponentConstructor('editor', class {
-  constructor(container: ComponentContainer) {
-    const data: any = {};
-    for (const key in navigator) {
-      data[key] = (navigator as any)[key];
-    }
-
-    const editor = new JSONEditor({
-      target: container.element,
-      props: {
-        mode: Mode.text,
-        mainMenuBar: true,
-        content: {
-          text: undefined,
-          json: data
-        }
+  new JSONEditor({
+    target: container.element,
+    props: {
+      mode: Mode.text,
+      mainMenuBar: true,
+      content: {
+        text: undefined,
+        json: data
       }
-    })
-  }
-}, false);
+    }
+  })
+});
 
-layout.registerComponentConstructor('editor2', class {
-  constructor(container: ComponentContainer) {
-    new EditorView({
-      doc: "Hello World",
-      extensions: [keymap.of(defaultKeymap), lineNumbers(), basicLight],
-      parent: container.element
-    });
-  }
-}, false);
+layout.registerComponentFactoryFunction('editor2', container => {
+  new EditorView({
+    doc: "Hello World",
+    extensions: [keymap.of(defaultKeymap), lineNumbers(), basicLight],
+    parent: container.element
+  });
+});
 
 layout.loadLayout(layoutConfig);
 
