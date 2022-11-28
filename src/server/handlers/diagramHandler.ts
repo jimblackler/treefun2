9e5eea4cb41783b950a86df483e4928ef409afe5
Jim Blackler <jimblackler@gmail.com>
2022-11-28 11:49:37 +0000

    Fix for canvg implementation for some versions of node.

--- src/server/handlers/diagramHandler.ts
+++ src/server/handlers/diagramHandler.ts
@@ -25,7 +25,8 @@ export const diagramHandler: RequestHandler = async (req, res, next) => {
     res.setHeader('Content-Type', 'image/png');
     const canvas = createCanvas(options.width, options.height);
     const ctx = canvas.getContext('2d');
-    const canvg = new Canvg(ctx, document, {DOMParser: window.DOMParser});
+    const canvg = new Canvg(ctx, document,
+        {DOMParser: window.DOMParser, fetch:() => {throw new Error()}});
     await canvg.render();
     const buffer = canvas.toBuffer('image/png');
     res.send(buffer);
