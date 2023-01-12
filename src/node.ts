aaa30c2603dbd939992dd736e694ef7e25426fd1
Jim Blackler <jimblackler@gmail.com>
2023-01-12 13:42:32 +0000

    Validation methods for trees.

--- src/node.ts
+++ src/node.ts
@@ -2,3 +2,23 @@ export type Node = {
   label: string;
   children?: Node[];
 }
+
+export function isNode(test: any): test is Node {
+  if (test === null) {
+    return false;
+  }
+  if (typeof test !== 'object') {
+    return false;
+  }
+  if (typeof test['label'] !== 'string') {
+    return false;
+  }
+  if (!('children' in test)) {
+    return true;
+  }
+  return isNodeArray(test.children);
+}
+
+export function isNodeArray(test: any): test is Node[] {
+  return Array.isArray(test) && test.every(isNode);
+}
