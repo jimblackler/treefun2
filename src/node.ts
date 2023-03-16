bf799620952bbb4746c51dfb9fe860dbe6a169eb
Jim Blackler <jimblackler@gmail.com>
2023-03-16 14:09:03 +0000

    Typescript type checker for tree.

--- src/node.ts
+++ src/node.ts
@@ -2,3 +2,20 @@ export type Node = {
   label: string;
   children?: Node[];
 }
+
+export function isNode(value: unknown): value is Node {
+  if (value === null || typeof(value) !== 'object' || Array.isArray(value)) {
+    return false;
+  }
+  if (!('label' in value) || typeof(value.label) !== 'string') {
+    return false;
+  }
+  if (!('children' in value)) {
+    return true;
+  }
+  return Array.isArray(value.children) && value.children.every(element => isNode(element));
+}
+
+export function isNodeArray(value: unknown): value is Node[] {
+  return Array.isArray(value) && value.every(entry => isNode(entry));
+}
